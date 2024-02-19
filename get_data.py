857118134347ff92eda2e056c912a3a014ba8e92
N4YuC4 <yucelcan028@gmail.com>
2024-02-19 13:01:45 +0300

    created main.py and get_data.py

---
+++ get_data.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+url_base="https://www.google.com/finance/quote/"
+
+def kur_al(birim):
+    response= requests.get(url_base+birim)
+    site_icerik = response.content
+
+    soup= BeautifulSoup(site_icerik,"html.parser")
+
+    return soup.find("div",class_="YMlKec fxKbKc").get_text()
+
